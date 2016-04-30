6cf31ee0b1c9487d5533933bfb3cb301dd7cd735
Aakash Sharma <aakash.y.sharma@gmail.com>
2016-04-30 15:56:46 +0200

    Added files for database access

--- DatabaseReader.py
+++ DatabaseReader.py
@@ -1,1 +1,23 @@
 #Read database for data about video request
+import yaml
+
+with open("db.yaml",'r') as dbconfig:
+    cfg = yaml.load(dbconfig)
+#test config file read
+print cfg['mysql']
+
+import MySQLdb
+#connect
+db = MySQLdb.connect(host="localhost", user="lab", passwd="labuser", db="lab_usage_data")
+
+cursor = db.cursor()
+
+cursor.execute("select * from requests")
+
+db.commit()
+
+numrows = int(cursor.rowcount)
+
+print numrows
+
+db.close()
