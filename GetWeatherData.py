8dc0491a4c23b29895e09cce2dcbf3dbe149984e
Aakash Sharma <aakash.y.sharma@gmail.com>
2016-05-08 14:44:57 +0200

    Added cities by population in France as cities in France has more internet penetration.

--- GetWeatherData.py
+++ GetWeatherData.py
@@ -7,10 +7,12 @@ with open("config.yaml",'r') as dbconfig:
 owm_details = cfg['owm']
 api_key = owm_details['api_key']
 owm = OWM(api_key)
+obs = owm.weather_history_at_place("Paris, France", start=1397426401,end=1398635589)
 
-obs = owm.weather_at_place('Paris, France')
+# obs = owm.weather_at_place('Paris, France')
+print obs
 w = obs.get_weather()
-
+# w.set_reference_time(1397935723506L)
 print w.get_clouds()
 print w.get_wind()
 print w.get_humidity()
