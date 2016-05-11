3a3c70f300b1338ccc826ff93066d7d1a83f112d
Aakash Sharma <aakash.y.sharma@gmail.com>
2016-05-11 20:05:22 +0200

    Added sample parser for data from NOAA

--- Weather.py
+++ Weather.py
@@ -1,1 +1,13 @@
-#Fetch weather data from PyOWM
+#Fetch weather data from NOAA
+# setup the connection
+from pydap.client import open_url
+
+
+url = 'http://nomads.ncdc.noaa.gov/dods/NCEP_NARR_DAILY/197901/197901/narr-a_221_197901dd_hh00_000'
+modelconn = open_url(url)
+tmp2m = modelconn['tmp2m']
+
+# grab the data
+lat_index = 200    # you could tie this to tmp2m.lat[:]
+lon_index = 200    # you could tie this to tmp2m.lon[:]
+print tmp2m.array[:,lat_index,lon_index]
